20784701ad0f1e01e2610ca20238fba5729b5c69
mategavran32 <m.gavran-student@globalsoft.ba>
2024-07-15 19:17:49 +0200

    small fix

--- backend/myApp/app/Http/Controllers/AuthController.php
+++ backend/myApp/app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -12,10 +11,10 @@ class AuthController extends Controller
     {
         $username = $request->input('username');
         $password = $request->input('password');
-        $query = "SELECT * FROM users WHERE username = ?";
-        $result = DB::select($query, [$username]);
-
-        if (empty($result) || !Hash::check($password, $result[0]->password)) {
+        $query = "SELECT * FROM users WHERE username = ? AND password = ?";
+        $result = DB::select($query, [$username, $password]);
+        
+        if (empty($result)) {
             return response()->json(['message' => 'User not found or password incorrect'], 200);
         }
         return response()->json($result);
@@ -25,9 +24,9 @@ class AuthController extends Controller
     {
         $username = $request->input('username');
         $email = $request->input('email');
-        $password = Hash::make($request->input('password'));
+        $password = $request->input('password');
         $query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-
+        
         DB::insert($query, [$username, $email, $password]);
         return response()->json(['message' => 'OK']);
     }
